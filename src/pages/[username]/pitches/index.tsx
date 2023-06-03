296cf23885c0e343ee2d7057f3b611168b4ea364
Hitesh Modi <hitesh_12014132@nitkkr.ac.in>
2023-06-04 00:32:48 +0530

    fixed

--- src/pages/[username]/pitches/index.tsx
+++ src/pages/[username]/pitches/index.tsx
@@ -3,15 +3,15 @@ import ProfileLayout from "components/layouts/profile-layout";
 import { useRouter } from "next/router";
 import { trpc } from "~/utils/trpc";
 
-const UserPitches = (props) => {
+const UserPitches = ({}) => {
   const router = useRouter();
   const username = router.query.username as string;
-  const getPosts=trpc.post.fetchAllPostsByUsername.useQuery({username});
+  const getPosts = trpc.post.fetchAllPostsByUsername.useQuery({ username });
   if (!getPosts.data) {
     // const businessProfile = getDetails(username);
     // if (businessProfile) return <About details={businessProfile} />;
     // else return notFound();
-    return <></>
+    return <></>;
   }
 
   return (

2a4a0ec9183a6e3e9eebeb9b664b300e499e25e1
Hitesh Modi <hitesh_12014132@nitkkr.ac.in>
2023-06-04 00:34:04 +0530

    fixed error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import { trpc } from "~/utils/trpc";
 import { setProgressBarState } from "~/utils/ui";
 
-export default function Home(props) {
+export default function Home({}) {
   const { data: session, status } = useSession();
   const router = useRouter();
   if (status === "loading") {
